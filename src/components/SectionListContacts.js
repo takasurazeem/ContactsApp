18ec2581891f947ccc5bd642aa85894c9f199112
Takausr Azeem <takasurazeem@gmail.com>
2020-04-30 14:38:14 +0500

    lecture init

--- src/components/SectionListContacts.js
+++ src/components/SectionListContacts.js
@@ -1,41 +1,44 @@
-import React from 'react'
-import { SectionList, Text } from 'react-native'
-import PropTypes from 'prop-types'
+import React from "react";
+import { SectionList, Text } from "react-native";
+import PropTypes from "prop-types";
 
-import Row from './Row'
+import Row from "./Row";
 
 // renderItem = obj => <Row { name={obj.item.name} phone={obj.item.phone}} />
 // item: { name: String, phone: String }
-const renderItem = ({ item }) => <Row {...item} />
+const renderItem = ({ item }) => <Row {...item} />;
 
-const renderSectionHeader = obj => <Text>{obj.section.title}</Text>
+const renderSectionHeader = (obj) => <Text>{obj.section.title}</Text>;
 
-const SectionListContacts = props => {
-    const contactsByLetter = props.contacts.reduce((obj, contact) => {
-        const firstLetter = contact.name[0].toUpperCase()
-        return {
-            ...obj,
-            [firstLetter]: [...(obj[firstLetter] || []), contact],
-        }
-    }, {})
+const SectionListContacts = (props) => {
+  const contactsByLetter = props.contacts.reduce((obj, contact) => {
+    const firstLetter = contact.name[0].toUpperCase();
+    return {
+      ...obj,
+      [firstLetter]: [...(obj[firstLetter] || []), contact],
+    };
+  }, {});
 
-    const sections = Object.keys(contactsByLetter).sort().map(letter => ({
-        title: letter,
-        data: contactsByLetter[letter],
-    }))
+  const sections = Object.keys(contactsByLetter)
+    .sort()
+    .map((letter) => ({
+      title: letter,
+      data: contactsByLetter[letter],
+    }));
 
-    return (
-        <SectionList
-            renderItem={renderItem}
-            renderSectionHeader={renderSectionHeader}
-            sections={sections} />
-    )
-}
+  return (
+    <SectionList
+      renderItem={renderItem}
+      renderSectionHeader={renderSectionHeader}
+      sections={sections}
+    />
+  );
+};
 
 SectionListContacts.propTypes = {
-    renderItem: PropTypes.func,
-    renderSectionHeader: PropTypes.func,
-    contacts: PropTypes.array,
-}
+  renderItem: PropTypes.func,
+  renderSectionHeader: PropTypes.func,
+  contacts: PropTypes.array,
+};
 
-export default SectionListContacts
+export default SectionListContacts;
